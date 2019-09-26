5434c9f92b808fc43506b53cba64857960b6bc17
Maeda Shoma <ec2-user@ip-172-31-27-225.us-east-2.compute.internal>
2019-09-26 11:52:59 +0000

    0926全体

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,3 +1,4 @@
+<!--
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,3 +12,21 @@
         <h1>Myニュース作成画面</h1>
     </body>
 </html>
+-->
+
+<!-- ↑10章　↓11章 -->
+
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+{{-- admin.blade.phpの＠yield('content')に以下のタグを埋め込む　--}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>ニュース新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+
